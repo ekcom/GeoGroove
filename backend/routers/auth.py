f9d4923c46a9fcfd697a41bd92bdc30b901fad09
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2024-06-26 22:40:23 -0500

    Remove spotipy lib

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Request
 from starlette.responses import RedirectResponse, FileResponse, JSONResponse
-import spotipy
 import os
 
 from utils.get_playlist_name import gen_playlist_name
@@ -15,7 +14,6 @@ router = APIRouter(
 def auth_sp():
     STATE = "TODO" # match with cookies or something
 
-    #sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(os.environ["CLIENT_ID"], os.environ["CLIENT_SECRET"], redirect_uri="http://localhost:8000/callback", scope="playlist-modify-private"))
     return RedirectResponse(url=f"https://accounts.spotify.com/authorize?client_id={os.environ['CLIENT_ID']}&response_type=token&redirect_uri={os.environ['ORIGIN_URL']}/callback&state={STATE}&scope=playlist-modify-private user-top-read")
 
 # Callback after Spotify auth
