20c5b9d1f717bc102283b27b786070a7de0a0929
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2024-06-26 23:57:31 -0500

    change from shiyuan

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { motion } from "framer-motion";
-import TextAnim from './TextAnim';
 import Hero from './hero';
 import Intro1 from './Intro1';
 import Intro2 from './intro2';
-// import Recommended from './Recommended';
 import footer from './assets/footer.svg'
-import Authentication from './authentication';
-import Success from './success';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
