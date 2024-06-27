491b027b042d2a9221f4de242e2c1a965dfa341d
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2024-06-26 20:12:45 -0500

    Add fastapi CLI usage hint

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
 
+if __name__ == "__main__":
+    print("Run this with `fastapi run app.py`")
+
 load_dotenv()
 
 app = FastAPI(docs_url=None)
